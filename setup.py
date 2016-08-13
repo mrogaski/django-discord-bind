84dd344dd19ae04b17aff0af1ef142d8380eaeb7
Mark Rogaski <mrogaski@pobox.com>
2016-08-13 12:12:46 -0400

    Edited version.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-discord-bind',
-    version='0.1.0dev',
+    version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',  # example license
