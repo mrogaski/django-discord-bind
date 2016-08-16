074944cc64e28de8c44df22407039af21c65218e
Mark Rogaski <mrogaski@pobox.com>
2016-08-16 00:43:57 -0400

    Added tests for index view.

---
+++ discord_bind/tests/test_views.py
@@ -0,0 +1,107 @@
+"""
+
+The MIT License (MIT)
+
+Copyright (c) 2016, Mark Rogaski
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+"""
+from __future__ import unicode_literals
+
+try:
+    from unittest.mock import patch, MagicMock
+except ImportError:
+    from mock import patch, MagicMock
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
+
+from django.test import TestCase, RequestFactory, override_settings
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.contrib.auth.models import User, AnonymousUser
+from django.core.urlresolvers import reverse
+from django.conf import settings
+
+from discord_bind.views import index, callback
+from discord_bind import app_settings
+
+
+class AuthorizationRequestTest(TestCase):
+    """ Test the authorization request view """
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username="Hoots",
+                                             email="test@example.com",
+                                             password="test")
+
+    @override_settings(DISCORD_CLIENT_ID='212763200357720576')
+    def test_get_index(self):
+        """ Test the index URL """
+        def user_request(user):
+            request = self.factory.get(reverse('discord_bind_index'))
+            request.user = user
+            middleware = SessionMiddleware()
+            middleware.process_request(request)
+            request.session.save()
+            return request
+
+        # Anonymous users should bounce to the login page
+        request = user_request(AnonymousUser())
+        response = index(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue('login' in response['location'])
+
+        # Loggied in users bounce to the auth request page
+        request = user_request(self.user)
+        response = index(request)
+        self.assertEqual(response.status_code, 302)
+        url = urlparse(response['location'])
+        self.assertEqual('https://discordapp.com/api/oauth2/authorize',
+                         url.scheme + '://' + url.netloc + url.path)
+        self.assertIn('response_type=code', url.query)
+        self.assertIn('client_id=212763200357720576', url.query)
+        self.assertIn('redirect_uri=http%3A%2F%2Ftestserver%2Fcb', url.query)
+        self.assertIn('scope=email+guilds.join', url.query)
+        self.assertIn('state=', url.query)
+
+        # Limited scope
+        with self.settings(DISCORD_EMAIL_SCOPE='212763200357720576'):
+            request = user_request(self.user)
+            response = index(request)
+            url = urlparse(response['location'])
+            self.assertIn('scope=identity+guilds.join', url.query)
+
+        # URI settings
+        with self.settings(DISCORD_BASE_URI='https://www.example.com/api'):
+            request = user_request(self.user)
+            response = index(request)
+            url = urlparse(response['location'])
+            self.assertEqual('', url.query)
+            self.assertEqual('https://www.example.com/api/oauth2/authorize',
+                             url.scheme + '://' + url.netloc + url.path)
+
+        with self.settings(DISCORD_AUTHZ_URI='/foo/bar'):
+            request = user_request(self.user)
+            response = index(request)
+            url = urlparse(response['location'])
+            self.assertEqual('', url.query)
+            self.assertEqual('https://discordapp.com/api/foo/bar',
+                             url.scheme + '://' + url.netloc + url.path)
