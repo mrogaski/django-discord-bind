06cb764a2983437b37f17c96b8404cd513b53c15
Mark Rogaski <mrogaski@pobox.com>
2016-08-18 23:49:31 -0400

    Expanded callback tests.

--- discord_bind/tests/test_callback_view.py
+++ discord_bind/tests/test_callback_view.py
@@ -37,10 +37,11 @@ import os
 
 from django.test import TestCase, RequestFactory, override_settings
 from django.contrib.sessions.middleware import SessionMiddleware
-from django.contrib.auth.models import User, AnonymousUser
+from django.contrib.auth.models import User, AnonymousUser, Group
 from django.core.urlresolvers import reverse
 
 from discord_bind.views import callback
+from discord_bind.models import DiscordInvite
 from discord_bind.conf import settings
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
@@ -53,6 +54,10 @@ class TestAccessTokenRequest(TestCase):
         self.user = User.objects.create_user(username="Ralff",
                                              email="ralff@example.com",
                                              password="test")
+        g = Group.objects.create(name='Discord Users')
+        g.user_set.add(self.user)
+        for code in ['code0', 'code1', 'code2', 'code3']:
+            DiscordInvite.objects.create(code=code, active=False)
 
     def tearDown(self):
         self.user.delete()
@@ -68,7 +73,7 @@ class TestAccessTokenRequest(TestCase):
             if query != '':
                 url = url + '?' + query
             request = self.factory.get(url)
-            
+
             # add user and session
             request.user = user
             middleware = SessionMiddleware()
@@ -79,7 +84,7 @@ class TestAccessTokenRequest(TestCase):
             request.session['discord_bind_return_uri'] = (
                     settings.DISCORD_RETURN_URI)
             request.session.save()
-            
+
             # build mock harness
             mock_fetch.return_value = {
                 "access_token": "tvYhMddlVlxNGPtsAN34w9P6pivuLG",
@@ -100,8 +105,8 @@ class TestAccessTokenRequest(TestCase):
             mock_response = mock.Mock()
             mock_response.json.return_value = user_data
             mock_get.return_value = mock_response
-            
-            # fire 
+
+            # fire
             return callback(request)
 
         # Anonymous users should bounce to the login page
@@ -110,7 +115,39 @@ class TestAccessTokenRequest(TestCase):
         self.assertTrue('login' in response['location'])
 
         # Discord user binding
-        response = get_callback(self.user, 
+        response = get_callback(self.user,
                                 'code=SplxlOBeZQQYbYS6WxSbIA&state=xyz')
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], settings.DISCORD_RETURN_URI)
+
+        # Missing code
+        response = get_callback(self.user,
+                                'state=xyz')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], settings.DISCORD_RETURN_URI)
+
+        # CSRF
+        response = get_callback(self.user,
+                                'code=SplxlOBeZQQYbYS6WxSbIA&state=abc')
+        self.assertEqual(response.status_code, 403)
+        response = get_callback(self.user,
+                                'error=server_error&state=abc')
+        self.assertEqual(response.status_code, 403)
+
+        # Missing state
+        response = get_callback(self.user,
+                                'code=SplxlOBeZQQYbYS6WxSbIA')
+        self.assertEqual(response.status_code, 403)
+        response = get_callback(self.user,
+                                'error=server_error')
+        self.assertEqual(response.status_code, 403)
+
+        # Valid error responses
+        for error in ['invalid_request', 'unauthorized_client',
+                      'access_denied', 'unsupported_response_type',
+                      'invalid_scope', 'server_error',
+                      'temporarily_unavailable']:
+            response = get_callback(self.user,
+                                    'error=%s&state=xyz' % error)
+            self.assertEqual(response.status_code, 302)
+            self.assertEqual(response['location'], settings.DISCORD_RETURN_URI)
