752a4ee17d69938680b32df3921244c9bac839fd
Mark Rogaski <mrogaski@pobox.com>
2018-09-10 00:35:17 -0400

    Converted model tests. (#7)

--- test_settings.py
+++ test_settings.py
@@ -1,28 +1,5 @@
-"""
-
-The MIT License (MIT)
-
-Copyright (c) 2016, Mark Rogaski
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-"""
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 
 DATABASES = {
     'default': {
@@ -42,3 +19,5 @@ INSTALLED_APPS = (
 SECRET_KEY = 'vn5v8g+q3q*ll)a3kh10wlj#(tc=738cklg9(z3***kw%qhnv-'
 
 ROOT_URLCONF = 'discord_bind.urls'
+
+DISCORD_CLIENT_ID = '212763200357720576'
